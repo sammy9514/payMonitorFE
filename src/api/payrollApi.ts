6aa5b3b8c90695bd3eafab797dbdcb8d87d12f00
Ayomide <ogunyemiayomide700@gmail.com>
2025-11-04 00:37:50 +0000

    small

--- src/api/payrollApi.ts
+++ src/api/payrollApi.ts
@@ -4,10 +4,13 @@ const url = import.meta.env.VITE_API_URL;
 
 export const createPayroll = async (pastWeeks: number, futureWeek: number) => {
   try {
-    const result: any = await axios.post(`${url}/payroll/create_payroll`, {
-      pastWeeks: pastWeeks,
-      futureWeek: futureWeek,
-    });
+    const result: any = await axios.post(
+      `${url}/api/v1/payroll/create_payroll`,
+      {
+        pastWeeks: pastWeeks,
+        futureWeek: futureWeek,
+      }
+    );
     return result.data.data;
   } catch (error) {
     console.log(error);
